dd8f19cd5cf5084c8475f20eb3ae0b54efa0b42b
pranavmahindru <pranavmahindru16@gmail.com>
2021-02-07 07:07:52 -0400

    done with the spinner so that user can see the account type and choose from the spinner.
    Done Sanju and Pranav

--- app/src/main/java/com/example/csci3130_project_group17/SignUp.java
+++ app/src/main/java/com/example/csci3130_project_group17/SignUp.java
@@ -3,15 +3,23 @@ package com.example.csci3130_project_group17;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class SignUp extends AppCompatActivity implements View.OnClickListener{
+import java.util.ArrayList;
+
+import static android.R.*;
+
+public class SignUp extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener{
 
 //    String firstName;
 //    String lastName;
@@ -33,6 +41,9 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener{
 
         //show the organization field for the employer
         showorganization();
+
+        //job show in spinner
+        jobsspinner();
 
         // take value of button
         Button square_button = (Button)findViewById(R.id.switchbutton);
@@ -85,6 +96,37 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener{
         });
     }
 
+    //---------------------------spinner start here -------------------------------------------------
+    //this thing is reference from the given link
+    //https://www.tutorialspoint.com/android/android_spinner_control.html
+    protected void jobsspinner(){
+        Spinner spinner = findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Account Type");
+        arrayList.add("individual");
+        arrayList.add("Business");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, layout.simple_spinner_item,arrayList){
+            @Override
+            public boolean isEnabled(int position) {
+                if (position == 0){
+                    return false;
+                }
+                return true;
+            }
+        };
+        arrayAdapter.setDropDownViewResource(layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
+    }
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+    //-------------------------------------------------end here--------------------------------------
 
 
     //email check if it is valid or not
