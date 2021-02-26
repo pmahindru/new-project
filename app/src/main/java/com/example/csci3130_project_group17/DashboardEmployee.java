3ff9bb61cc5a18633cc87bdc807db56705082e73
Sanjuice <sn696414@dal.ca>
2021-02-25 23:05:56 -0400

    Add dashboard activities -Sanjana

---
+++ app/src/main/java/com/example/csci3130_project_group17/DashboardEmployee.java
@@ -0,0 +1,14 @@
+package com.example.csci3130_project_group17;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DashboardEmployee extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard_employee);
+    }
+}
