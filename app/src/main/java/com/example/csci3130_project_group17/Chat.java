6a8ab457a3f34e08a7efd52af9efa29fe26f181c
tliu <tn934739@dal.ca>
2021-03-09 17:30:32 -0400

    change onclick method,still have question on display message method

--- app/src/main/java/com/example/csci3130_project_group17/Chat.java
+++ app/src/main/java/com/example/csci3130_project_group17/Chat.java
@@ -13,18 +13,17 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.firebase.ui.database.FirebaseListAdapter;
-import com.firebase.ui.database.FirebaseListOptions;
+
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
+
 import com.google.firebase.database.FirebaseDatabase;
 
 public class Chat extends AppCompatActivity {
     private Button SendButton, ButtonHome;
     private View input;
-    DatabaseReference ChatInformation;
+
 
     ChatMessage message;
-    private FirebaseListAdapter<ChatMessage> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,23 +34,27 @@ public class Chat extends AppCompatActivity {
         ButtonHome = findViewById(R.id.Home);
         message = new ChatMessage();
 
-        initializedatabase();
     }
 
-    //inital database
-    public void initializedatabase(){
-        ChatInformation = FirebaseDatabase.getInstance().getReference();
-    }
+
 
     protected void switchToMain(){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 
-    public void onClick(View view) {
+    public void onClick(View v){
+        ButtonHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchToMain();
 
-        switch (view.getId()){
-            case R.id.Send:
+            }
+        });
+
+        SendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 EditText input = (EditText)findViewById(R.id.textInput);
 
                 // Read the input field and push a new instance
@@ -68,13 +71,8 @@ public class Chat extends AppCompatActivity {
                 // Clear the input
                 input.setText("");
                 displayChatMessages();
-
-            case R.id.Home:
-                switchToMain();
-                break;
-
-        }
-
+            }
+        });
     }
 
 
@@ -82,15 +80,16 @@ public class Chat extends AppCompatActivity {
         ListView listOfMessages = (ListView)findViewById(R.id.chatcontent);
 
 
-
-        FirebaseListAdapter<ChatMessage>firebaseListAdapter = new FirebaseListAdapter<ChatMessage>(Chat.this,ChatMessage.class,
-                android.R.layout.simple_list_item_1,FirebaseDatabase.getInstance().getReference()) {
+        // Get references to the views of message.xml
+        // Set their text
+        FirebaseListAdapter<ChatMessage> adapter = new FirebaseListAdapter<ChatMessage>(this, ChatMessage.class,
+               android.R.layout.simple_list_item_activated_2, FirebaseDatabase.getInstance().getReference()) {
             @Override
             protected void populateView(@NonNull View v, @NonNull ChatMessage model, int position) {
                 // Get references to the views of message.xml
-                TextView messageText = (TextView)v.findViewById(R.id.message_text);
-                TextView messageUser = (TextView)v.findViewById(R.id.message_user);
-                TextView messageTime = (TextView)v.findViewById(R.id.message_time);
+                TextView messageText = (TextView) v.findViewById(R.id.message_text);
+                TextView messageUser = (TextView) v.findViewById(R.id.message_user);
+                TextView messageTime = (TextView) v.findViewById(R.id.message_time);
 
                 // Set their text
                 messageText.setText(model.getMessageText());
@@ -100,7 +99,7 @@ public class Chat extends AppCompatActivity {
             }
         };
 
-        listOfMessages.setAdapter(firebaseListAdapter);
+        listOfMessages.setAdapter(adapter);
     }
 
 
