8c7b05e249c7be312ab66d7331c0b4f8fccb1ca3
Sabrina Dasilva <sb296761@dal.ca>
2021-03-08 23:14:23 -0400

    add switch to jobPosting functionality -sabrina

--- app/src/main/java/com/example/csci3130_project_group17/DashboardEmployer.java
+++ app/src/main/java/com/example/csci3130_project_group17/DashboardEmployer.java
@@ -2,7 +2,10 @@ package com.example.csci3130_project_group17;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DashboardEmployer extends AppCompatActivity {
 
@@ -14,6 +17,21 @@ public class DashboardEmployer extends AppCompatActivity {
         //get stored user data
         StoredData data = new StoredData(getApplicationContext());
         String uID = data.getStoredUserID();
+        setClickListeners();
+    }
+
+    private void setClickListeners() {
+        Button createJobButton= (Button) findViewById(R.id.employerCreateJobButton);
+        createJobButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                switchToCreateJob();
+            }
+        });
+    }
+    private void switchToCreateJob(){
+        Intent CreateJobIntent = new Intent(this, JobPosting.class);
+        startActivity(CreateJobIntent);
+
     }
 
 }
