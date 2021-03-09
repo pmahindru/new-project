3eb5d267d2f06a0d699bf8772bdbddb5558a8f52
Sabrina Dasilva <sb296761@dal.ca>
2021-03-08 23:18:50 -0400

    change jobPosting to use job class and add state and employerid fields to database when job created -sabrina

--- app/src/main/java/com/example/csci3130_project_group17/JobPosting.java
+++ app/src/main/java/com/example/csci3130_project_group17/JobPosting.java
@@ -18,7 +18,7 @@ public class JobPosting extends AppCompatActivity implements View.OnClickListene
     private Button createButton, ButtonHome;
     private String errorMessage;
     DatabaseReference jobInformation;
-
+    String uID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +26,8 @@ public class JobPosting extends AppCompatActivity implements View.OnClickListene
         setContentView(R.layout.activity_job_posting);
 
         Intent jobIntent = getIntent();
+        StoredData data = new StoredData(getApplicationContext());
+        uID = data.getStoredUserID();
 
         createButton = findViewById(R.id.createJobButton);
         createButton.setOnClickListener(this);
@@ -127,15 +129,12 @@ public class JobPosting extends AppCompatActivity implements View.OnClickListene
 
     protected void saveJobToDatabase(){
         String jobID = UUID.randomUUID().toString();
-        jobInformation.child(jobID).child("jobTitle").setValue(getJobTitle());
-        jobInformation.child(jobID).child("jobType").setValue(getJobType());
-        jobInformation.child(jobID).child("jobPayRate").setValue(getJobPayRate());
-        jobInformation.child(jobID).child("jobLocation").setValue(getJobLocation());
-        jobInformation.child(jobID).child("jobDescription").setValue(getJobDescription());
+        Job job = new Job(getJobTitle(),getJobType(),getJobDescription(),getJobLocation(),getJobPayRate(),"open","",uID);
+        jobInformation.child(jobID).setValue(job);
     }
 
     protected void switchToMain(){
-        Intent intent = new Intent(this, MainActivity.class);
+        Intent intent = new Intent(this, DashboardEmployer.class);
         startActivity(intent);
     }
 
