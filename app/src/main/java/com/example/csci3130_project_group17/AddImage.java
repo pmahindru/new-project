001ddd48ae6f40c37385bbffbd2c1b9fe638929d
unknown <you@example.com>
2021-03-09 12:05:08 -0400

    main program of add image function Jialun Wen

--- app/src/main/java/com/example/csci3130_project_group17/AddImage.java
+++ app/src/main/java/com/example/csci3130_project_group17/AddImage.java
@@ -1,14 +1,103 @@
 package com.example.csci3130_project_group17;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import java.io.IOException;
 import android.os.Bundle;
 
 public class AddImage extends AppCompatActivity {
-
+    Button UploadIMG, ChooseIMG;
+    ImageView imageView;
+    Uri FilePathUri;
+    StorageReference storageReference;
+    DatabaseReference databaseReference;
+    int Image_Request_Code = 7;
+    EditText text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_image);
+        storageReference = FirebaseStorage.getInstance().getReference("jobImages");
+        databaseReference = FirebaseDatabase.getInstance().getReference("jobImages");
+        ChooseIMG = (Button) findViewById(R.id.ChooseIMG);
+        UploadIMG = (Button) findViewById(R.id.UploadIMG);
+        text = (EditText) findViewById(R.id.text);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        ChooseIMG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Filechooser();
+            }
+        });
+        UploadIMG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Fileuploader();
+            }
+        });
+
+    }
+    private String getExtension(Uri uri){
+        ContentResolver contentResolver = getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri)) ;
+    }
+    private void Fileuploader(){
+        StorageReference reference = storageReference.child(System.currentTimeMillis()+"."+getExtension(FilePathUri));
+        reference.putFile(FilePathUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                String TempImageName=text.getText().toString().trim();
+                Toast.makeText(getApplicationContext(),"Image Successfully Uploaded", Toast.LENGTH_LONG).show();
+                String ImageUploadId = databaseReference.push().getKey();
+                databaseReference.child(ImageUploadId).setValue(imageView);
+            }
+        });
+    }
+    private void Filechooser(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Image"), Image_Request_Code);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == Image_Request_Code && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            FilePathUri = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), FilePathUri);
+                imageView.setImageBitmap(bitmap);
+            }
+            catch (IOException e) {
+
+                e.printStackTrace();
+            }
+        }
     }
 }
