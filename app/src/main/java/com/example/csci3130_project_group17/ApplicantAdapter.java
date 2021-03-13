81a2f220251d618edbd003af16c854c574436359
Ryan Hickman <ry627284@dal.ca>
2021-03-13 18:28:41 -0400

    Created the recycler view and card to display applicants. Also created applicant class, Applicant adapter to feed information to the applicant card and connect the card and recycler. - Ryan Hickman B00821827

---
+++ app/src/main/java/com/example/csci3130_project_group17/ApplicantAdapter.java
@@ -0,0 +1,49 @@
+package com.example.csci3130_project_group17;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+
+public class ApplicantAdapter extends FirebaseRecyclerAdapter<Applicant, ApplicantAdapter.ApplicantsViewHolder>{
+
+    public ApplicantAdapter(@NonNull FirebaseRecyclerOptions<Applicant> options){
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull @org.jetbrains.annotations.NotNull ApplicantsViewHolder holder, int position, @NonNull @org.jetbrains.annotations.NotNull Applicant model) {
+
+        holder.firstName.setText(model.getFirstName());
+
+        holder.lastName.setText(model.getLastName());
+    }
+
+
+    @NonNull
+    @org.jetbrains.annotations.NotNull
+    @Override
+    public ApplicantAdapter.ApplicantsViewHolder onCreateViewHolder(@NonNull @org.jetbrains.annotations.NotNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.applicant_card, parent, false);
+
+        return new ApplicantAdapter.ApplicantsViewHolder(view);
+    }
+
+    class ApplicantsViewHolder extends RecyclerView.ViewHolder {
+        TextView firstName, lastName;
+
+        public ApplicantsViewHolder(@NonNull View itemView){
+            super(itemView);
+
+            firstName = itemView.findViewById(R.id.applicantFirstName);
+            lastName = itemView.findViewById(R.id.applicantLastName);
+        }
+    }
+}
