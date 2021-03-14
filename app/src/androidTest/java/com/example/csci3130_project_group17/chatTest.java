33e6a15cfbaf9740199dd102b2c8ff4d9ccf3d35
Sanjana Swami <sn696414@dal.ca>
2021-03-14 19:47:35 -0300

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 974d5c4b9b126bcbe49142040e14d489f3e831bc

--- app/src/androidTest/java/com/example/csci3130_project_group17/chatTest.java
+++ app/src/androidTest/java/com/example/csci3130_project_group17/chatTest.java
@@ -2,29 +2,19 @@ package com.example.csci3130_project_group17;
 
 import android.content.Context;
 
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
-    @Test
+public class chatTest {
+
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.csci3130_project_group17", appContext.getPackageName());
     }
-
-    //this is based on the acceptance test.
-
 }
-
